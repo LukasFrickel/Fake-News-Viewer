6d117abeb7777ebec132f232dba2e1ef6bb414a1
Fvrneto <rielneto@hhu.de>
2022-02-02 23:45:40 +0100

    Francisco POS, Stopwords, NE add to main code

--- Fake-News-Viewer-Tests.py
+++ Fake-News-Viewer-Tests.py
@@ -1,23 +1,27 @@
 #Created by Francisco van Riel Neto
 
+############################ Importing necessaire modelos
+
 import pandas as pd
 import nltk
 from nltk import word_tokenize
-from nltk.tokenize import blankline_tokenize
+from nltk.corpus import stopwords
+#from nltk.tokenize import blankline_tokenize
 from nltk.probability import FreqDist
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS
+#from nltk.util import bigrams, trigrams, ngrams
+from nltk.stem import SnowballStemmer
+import string
+from nltk import ne_chunk 
 
 #nltk.download('all')
 
-#path_train = '//gilch.ad.hhu.de/Daten/Home/Riel Neto/Privat/TechLabs/fake-news/train.csv'
 path_train = 'C:/Users/franc/Desktop/TechLabs/fake-news/train.csv'
 
 df = pd.read_csv(path_train)
 df = df[df['text'].notna()]
 df.reset_index(drop=True, inplace=True)
 
-# cleaning data and spliting datasets:
+############################ cleaning data:
     
 df['title'] = df['title'].fillna('None')
 df['author'] = df['author'].fillna('None')
@@ -26,9 +30,26 @@ df.reset_index(drop=True, inplace=True)
 
 print(df.isna().sum())
 
+############################ removing stopwords (did it after tokanization)
+
+stopw = stopwords.words('english')
+
+#print(stopw)
+
+#df_stopwords = df
+
+#df_stopwords['title'] = df['title'].str.lower()
+#df_stopwords['title'] = df_stopwords['title'].apply(lambda x: ' '.join([word for word in 
+#                                                      x.split() if word not in (stopw)]))
+
+#df_stopwords['text'] = df['text'].str.lower()
+#df_stopwords['text'] = df_stopwords['text'].apply(lambda x: ' '.join([word for word in 
+#                                                      x.split() if word not in (stopw)]))  
+#spliting datasets:
+
 df_reliable = df.drop(df[df.label == 0].index)
 df_unreliable = df.drop(df[df.label == 1].index)
-
+                            
 # tokenization and spliting datasets:
     
 df_token = df
@@ -36,13 +57,45 @@ df_token = df
 df_token['title'] = df_token['title'].apply(word_tokenize)
 df_token['author'] = df_token['author'].apply(word_tokenize)
 df_token['text'] = df_token['text'].apply(word_tokenize)
+  
+############################ punctuation and stopword
 
-#spliting the data into reliable and unreliable
+punctuation = string.punctuation
+
+def Convert(string):
+    list1=[]
+    list1[:0]=string
+    return list1
+punctuation = (Convert(punctuation))
+punctuation.append("’")
+
+stop_punc = stopw + punctuation
+
+n = 0
+
+for n in range(len(df_token['title'])):
+    for word in df_token.iloc[n]['title']:
+        if word in stop_punc:
+            df_token.iloc[n]['title'].remove(word)
+            #print(word)
+    n +=1 
+     
+    
+n = 0
+
+for n in range(len(df_token['text'])):
+    for word in df_token.iloc[n]['text']:
+        if word in stop_punc:
+            df_token.iloc[n]['text'].remove(word)  
+            #print(word)
+    n +=1 
+     
+############################ spliting the data into reliable and unreliable
 
 df_token_reliable = df_token.drop(df[df.label == 0].index)
 df_token_unreliable = df_token.drop(df[df.label == 1].index)
-
-#word frequencies
+    
+############################ word frequencies in titles
 
 fdist_title_reliable = FreqDist()
 counter = 0
@@ -62,39 +115,32 @@ for counter in range(len(df_token_unreliable['title'])):
 
 print (fdist_title_reliable.most_common(10))      
 print (fdist_title_unreliable.most_common(10))
-     
-# number of paragraphs
+
+############################ Stemming
+
+stemmer = SnowballStemmer("english")
+
+df_token_reliable_stemmed = df_token_reliable['title'].apply(lambda x: [stemmer.stem(y) for y in x])  # Stem every word.
+df_token_unreliable_stemmed = df_token_unreliable['title'].apply(lambda x: [stemmer.stem(y) for y in x])  # Stem every word.
+
+############################ POS - Parts of Speech
+
+df_token_reliable_pos = df_token_reliable
+df_token_reliable_pos['title'] = df_token_reliable_pos['title'].apply(nltk.pos_tag)
+df_token_reliable_pos['text'] = df_token_reliable_pos['text'].apply(nltk.pos_tag)
 
 
-print(blankline_tokenize(df_token_reliable['title']))
-print(blankline_tokenize(df_token_unreliable['title']))
+df_token_unreliable_pos = df_token_unreliable
+df_token_unreliable_pos['title'] = df_token_unreliable_pos['title'].apply(nltk.pos_tag)
+df_token_unreliable_pos['text'] = df_token_unreliable_pos['text'].apply(nltk.pos_tag)
 
-# word cloud plot:
+############################ Name ententity
 
-df_reliable_text = ' '.join(df_reliable['text'].tolist())
-df_unreliable_text = ' '.join(df_unreliable['text'].tolist())
+df_token_reliable_ne = df_token_reliable_pos
+df_token_reliable_ne['title'] = df_token_reliable_ne['title'].apply(ne_chunk)
+df_token_reliable_ne['text'] = df_token_reliable_ne['text'].apply(ne_chunk)
 
-word_cloud_true = WordCloud(max_font_size=50, max_words=100, 
-                            collocations = False, 
-                            stopwords=STOPWORDS, 
-                            background_color = 'white', 
-                            width=400, 
-                            height=300).generate(df_reliable_text)
 
-word_cloud_fake = WordCloud(max_font_size=50, 
-                            max_words=100, 
-                            collocations = False, 
-                            stopwords=STOPWORDS, 
-                            background_color = 'black', 
-                            width=400,
-                            height=300).generate(df_unreliable_text)
-
-fig, ax = plt.subplots(1, 2, figsize  = (30,30))
-
-ax[0].imshow(word_cloud_true, interpolation = 'bilinear')
-ax[0].set_title('Word Cloud Reliable', fontsize=30)
-ax[0].axis('off')   
-     
-ax[1].imshow(word_cloud_fake, interpolation = 'bilinear')
-ax[1].set_title('Word Cloud Unreliable', fontsize=30)
-ax[1].axis('off')
+df_token_unreliable_ne = df_token_unreliable_pos
+df_token_unreliable_ne['title'] = df_token_unreliable_ne['title'].apply(ne_chunk)
+df_token_unreliable_ne['text'] = df_token_unreliable_ne['text'].apply(ne_chunk)
