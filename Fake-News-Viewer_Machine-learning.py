5b3326561ae2b01deebfe17739195c6eecc755cf
Fvrneto <rielneto@hhu.de>
2022-03-07 00:04:53 +0100

    Machine learning more modules implemented

--- Fake-News-Viewer_Machine-learning.py
+++ Fake-News-Viewer_Machine-learning.py
@@ -11,24 +11,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
 
 ############################ Importing user modules
 
 import Modules_User.cleaning as cleaning 
 
+
+############################ 
 path_train = 'C:/Users/franc/Desktop/TechLabs/fake-news/train.csv'
 
-############################ removing stopwords, numbers and punctuations
-
-ath_train = 'C:/Users/franc/Desktop/TechLabs/fake-news/train.csv'
-
 df = pd.read_csv(path_train)
-df = df[df['text'].notna()]
-df.reset_index(drop=True, inplace=True)
 
 ############################ dropping nan:
     
+df = df[df['text'].notna()]
+df.reset_index(drop=True, inplace=True)
+
 df['title'] = df['title'].fillna('None')
 df['author'] = df['author'].fillna('None')
 df = df[df['text'].notna()]
@@ -36,23 +38,23 @@ df.reset_index(drop=True, inplace=True)
 
 ############################ removing stopwords, numbers and punctuations
 
-df_stopwords = df
+df_clean = df
 
-df_stopwords['title'] = df_stopwords['title'].apply(lambda x: cleaning.clean_numbers(x))
-df_stopwords['title'] = df_stopwords['title'].apply(cleaning.clean_steapwords())
-df_stopwords['title'] = df_stopwords['title'].apply(lambda x: cleaning.clean_punctuations(x))
+df_clean['title'] = df_clean['title'].apply(lambda x: cleaning.clean_numbers(x))
+df_clean['title'] = df_clean['title'].apply(cleaning.clean_steapwords())
+df_clean['title'] = df_clean['title'].apply(lambda x: cleaning.clean_punctuations(x))
 
 
-df_stopwords['text'] = df_stopwords['text'].apply(lambda x: cleaning.clean_numbers(x))
-df_stopwords['text'] = df_stopwords['text'].apply(cleaning.clean_steapwords())
-df_stopwords['text'] = df_stopwords['text'].apply(lambda x: cleaning.clean_punctuations(x))
+df_clean['text'] = df_clean['text'].apply(lambda x: cleaning.clean_numbers(x))
+df_clean['text'] = df_clean['text'].apply(cleaning.clean_steapwords())
+df_clean['text'] = df_clean['text'].apply(lambda x: cleaning.clean_punctuations(x))
 
 ############################ Prepare for Machine Learning: Count Vectorizer
 
 cv = TfidfVectorizer(min_df=1)
 
-df_X = cv.fit(df_stopwords['text'])
-df_X = cv.transform(df_stopwords['text'])
+df_X = cv.fit(df_clean['text'])
+df_X = cv.transform(df_clean['text'])
 
 print('Full vector: ')
 print(df_X.toarray)
@@ -61,13 +63,43 @@ df_y = df["label"].values
 
 X_train, X_test, y_train, y_test = train_test_split(df_X, df_y, random_state=0)
 
-mnb = MultinomialNB()
 
-mnb.fit(X_train, y_train)
-print(mnb)
+Models = [LogisticRegression(),
+          MultinomialNB(),
+          RandomForestClassifier(),
+          DecisionTreeClassifier(),
+          AdaBoostClassifier()]
 
-y_pred = mnb.predict(X_test)
-y_expect = y_test
+Model_accuracy = []
+Model_test = []
+                   
+for model_name in Models:
+    
+    model_name.fit(X_train, y_train)
+    print(model_name)
 
-print (accuracy_score(y_expect, y_pred))
+    y_pred = model_name.predict(X_test)
+    y_expect = y_test
+    accuracy_model = accuracy_score(y_expect, y_pred)
+    Model_accuracy.append(accuracy_model)
+    print (accuracy_model)
+
+    y_pred_test = model_name.predict(X_train)
+    y_expect_test = y_train
+    accuracy_test = accuracy_score(y_expect_test, y_pred_test)
+    Model_test.append(accuracy_test)
+    print (accuracy_test)
+    
+    
+df_models = pd.DataFrame({'Models': Models, 
+                         'Accuracy': Model_accuracy , 
+                         'Test': Model_test, 
+                         })
+
+
+
+
+
+
+
 
