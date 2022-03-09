f26453a975947cdc12c73b22b5429b0141aa452b
Fvrneto <rielneto@hhu.de>
2022-03-09 23:32:20 +0100

    machine learning improvement attempt

--- Fake-News-Viewer_Machine-learning.py
+++ Fake-News-Viewer_Machine-learning.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 21:32:32 2022
+
+@author: franc
+"""
 
 import pandas as pd
 
@@ -10,21 +15,23 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
+import pickle
+
+############################ download stopwords
+
+import nltk
+nltk.download('stopwords')
 
 ############################ Importing user modules
 
 import Modules_User.cleaning as cleaning 
 
-
 ############################ 
-path_train = 'C:/Users/franc/Desktop/TechLabs/fake-news/train.csv'
+path_train = 'C:/Users/franc/Desktop/TechLabs/GitHub/Fake-News-Viewer/fake-news/train.csv'
 
 df = pd.read_csv(path_train)
 
 ############################ dropping nan:
-    
-df = df[df['text'].notna()]
-df.reset_index(drop=True, inplace=True)
 
 df['title'] = df['title'].fillna('None')
 df['author'] = df['author'].fillna('None')
@@ -34,11 +41,6 @@ df.reset_index(drop=True, inplace=True)
 ############################ removing stopwords, numbers and punctuations
 
 df_clean = df
-
-df_clean['title'] = df_clean['title'].apply(lambda x: cleaning.clean_numbers(x))
-df_clean['title'] = df_clean['title'].apply(cleaning.clean_steapwords())
-df_clean['title'] = df_clean['title'].apply(lambda x: cleaning.clean_punctuations(x))
-
 
 df_clean['text'] = df_clean['text'].apply(lambda x: cleaning.clean_numbers(x))
 df_clean['text'] = df_clean['text'].apply(cleaning.clean_steapwords())
@@ -71,30 +73,72 @@ Model_test = []
 for model_name in Models:
     
     model_name.fit(X_train, y_train)
-    print(model_name)
 
     y_pred = model_name.predict(X_test)
     y_expect = y_test
     accuracy_model = accuracy_score(y_expect, y_pred)
     Model_accuracy.append(accuracy_model)
-    print (accuracy_model)
 
     y_pred_test = model_name.predict(X_train)
     y_expect_test = y_train
     accuracy_test = accuracy_score(y_expect_test, y_pred_test)
-    Model_test.append(accuracy_test)
-    print (accuracy_test)
-    
+    Model_test.append(accuracy_test) 
     
 df_models = pd.DataFrame({'Models': Models, 
                          'Accuracy': Model_accuracy , 
                          'Test': Model_test, 
                          })
 
+print(df_models)
+
+############################ Using best model = LogisticRegression() to predict user text
+
+best_model = LogisticRegression()
+best_model.fit(X_train, y_train)
+
+############################ save the model to disk
+
+filename = 'finalized_lG_model.sav'
+pickle.dump(best_model , open(filename, 'wb'))
+
+############################ load the model from disk
+
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, y_test)
+
+print(result)
+
+############################ user text
+
+path_user_fake = 'C:/Users/franc/Desktop/TechLabs/GitHub/Fake-News-Viewer/other_data_set/archive/Fake.csv'
+path_user_true = 'C:/Users/franc/Desktop/TechLabs/GitHub/Fake-News-Viewer/other_data_set/archive/True.csv'
+
+df_user_true = pd.read_csv(path_user_true)
+df_user_fake = pd.read_csv(path_user_fake)
+
+############################ cleaning process
+
+df_user_true = df_user_true["text"].apply(cleaning.clean_steapwords())
+df_user_fake = df_user_fake["text"].apply(cleaning.clean_steapwords())
+
+df_user_true = df_user_true.iloc[0]
+df_user_fake = df_user_fake.iloc[0]
+
+df_user_true = cleaning.clean_punctuations(df_user_true)
+df_user_true = cleaning.clean_punctuations(df_user_true)
 
 
+df_user_fake = cleaning.clean_punctuations(df_user_fake)
+df_user_fake = cleaning.clean_punctuations(df_user_fake)
 
+############################ predict user text
 
+cv = TfidfVectorizer(min_df=1)
 
+df_user_true = [df_user_true]
+df_user_fake = [df_user_fake]
 
+X_user_true = cv.fit(df_user_true)
+X_user_true = cv.transform(df_user_true)
 
+ynew_true = loaded_model.predict(X_user_true)
